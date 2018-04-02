4e56af9bbefd6fd5636eece11a51fe2b04759063
github username <github email address>
2018-04-02 15:18:19 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function produceDrivingRange(range) {
+  // console.log(range)
+  return function(b1, b2) {
+    const distance = Math.abs(parseInt(b1)-parseInt(b2))
+    // console.log(distance)
+    // console.log(`1`, range)
+    // console.log(`2`, b1, b2)
+    if (range < distance) {
+      return `${distance - range} blocks out of range`
+    } else {
+      return `within range by ${range - distance}`
+    }
+  }
+}
+
+function produceTipCalculator(percent) {
+  console.log(percent)
+  return function(amount) {
+    console.log(amount)
+    return amount * percent
+  }
+}
